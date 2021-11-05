34c4f9b182b8d7e0c7e4dc0b24091ed9676cf30f
marcossalas1994 <70782971+marcossalas1994@users.noreply.github.com>
2021-11-05 19:03:28 -0300

    alguos cambios marcos

--- tp4/Estado de cuenta.cs
+++ tp4/Estado de cuenta.cs
@@ -8,24 +8,53 @@ namespace tp4
 {
     public class EstadoCuenta
     {
-        public int codigo_operacion { get; set; }
         public int codigo_cliente { get; set; }
+        public int codigo_operacion { get; set; }
         public DateTime fecha_operacion { get; set; }
         public string concepto { get; set; }
         public int cargos { get; set; }
         public int abonos { get; set; }
+        public List<EstadoCuenta> ListaEstadoCuentaPorCliente { get; set; }
 
-
-        public EstadoCuenta(int Codigo_operacion, int Codigo_cliente, DateTime Fecha_operacion, string Concepto, int Cargos,  int Abonos)
+        
+/*        public EstadoCuenta(int Codigo_cliente, int Codigo_operacion,  DateTime Fecha_operacion, string Concepto, int Cargos,  int Abonos)
         {
-            codigo_operacion = Codigo_operacion;
             codigo_cliente = Codigo_cliente;
+            codigo_operacion = Codigo_operacion;
             fecha_operacion = Fecha_operacion;
             concepto = Concepto;
             cargos = Cargos;
             abonos = Abonos;
         }
+ */       
+        public static void carga_prueba_estadocuenta()
+        {
+            //Un método que permite cargar un cliente en json, para poder realizar un prueba de carga por consola
+            var a = new EstadoCuenta();
+            a.codigo_operacion = 12;
+            a.codigo_cliente = 1;
+            a.fecha_operacion = DateTime.Now;
+            a.concepto = "calle falsa 123";
+            a.cargos = 10000;
+            a.abonos = 10000;
 
+            List<EstadoCuenta> lista = new List<EstadoCuenta>();
+            lista.Add(a);
+           
+
+
+        }
+
+   /*     public static List<EstadoCuenta> abrir_archivo()
+        {
+            string estadoCuentaJson = File.ReadAllText("estadoCuenta.Json");
+
+            List<EstadoCuenta> lista = JsonConvert.DeserializeObject<List<EstadoCuenta>>(estadoCuentaJson);
+
+            return lista;
+
+        }
+   */
         private void  ValidarClaveSecreta(int clave_secreta)
         {
             Console.WriteLine("A implementar");
@@ -43,7 +72,7 @@ namespace tp4
             Console.WriteLine("A implementar");
         }
 
-
+        
 
     }
 }
