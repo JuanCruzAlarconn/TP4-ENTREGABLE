661c5571af5b4087e9f1e2b1bbb320116c4b31f2
JuanCruzAlarconn <94AL38456910@campus.economicas.uba.ar>
2021-11-08 21:33:00 -0300

    comentario

--- tp4/AppCrearOrdenes.cs
+++ tp4/AppCrearOrdenes.cs
@@ -80,10 +80,10 @@ namespace tp4
                 int codigo = ordenes.asignar_codigo_servicio();
                 string estado = "Inicializado";
                 DateTime fecha = DateTime.Now;
-                ordenes.agregar(new Orden_de_servicio2(codigo, cliente, origen, destino, modalidad, costo, estado, fecha));
+                ordenes.agregar(new Orden_de_servicio2(codigo, cliente, origen, destino, modalidad, costo, estado, fecha));//falta paquete
                 Console.WriteLine("Orden de servicio generada Nro:" + codigo);
                 //METODO PARA GRABAR
-
+                //METODO PARA PASAR OBJETO A ESTADO DE CUENTA
             }
 
 
